b2c150f276881e969dab1d118774c5fa632a1614
Thomas Klijnsma <tklijnsm@gmail.com>
2021-04-19 10:58:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Black Box Energy Flow Polynomial determination
+
+Following https://arxiv.org/abs/2010.11998 .
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name          = 'bbefp',
+    version       = '0.1',
+    license       = 'BSD 3-Clause License',
+    description   = 'Description text',
+    url           = 'https://github.com/tklijnsma/bbefp.git',
+    author        = 'Thomas Klijnsma',
+    author_email  = 'tklijnsm@gmail.com',
+    packages      = ['bbefp'],
+    zip_safe      = False,
+    # scripts       = [],
+    )
